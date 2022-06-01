0c83278d5fd3d2b57a69c76afaacd6d2558ae2ea
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 23:07:32 +0900

    Standardize

--- 16_cons&prof.py
+++ 16_cons&prof.py
@@ -1,5 +1,4 @@
 #Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
-
 #Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
 
 with open(".\\16_original.txt") as f:
