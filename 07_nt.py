f26a099f06fa406690c037a26ce5184fe6f7adc2
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-02 00:07:13 +0900

    Update 07_nt.py

--- 07_nt.py
+++ 07_nt.py
@@ -1,6 +1,6 @@
 #Given: A DNA string s of length at most 1000 nt.
 #Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 
-o = open("G:\\My Drive\\PyPractice\\Rosalind\\7_original.txt","r")
+o = open("G:\\My Drive\\PyPractice\\Rosalind\\07_original.txt","r")
 s = o.read()
 print(s.count("A"),s.count("C"),s.count("G"),s.count("T"))
