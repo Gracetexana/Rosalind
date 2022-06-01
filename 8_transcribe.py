520222203d2bc072aa2f8147d79d92f2e813a3d1
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 22:55:05 +0900

    Clean up

--- 8_transcribe.py
+++ 8_transcribe.py
@@ -2,6 +2,4 @@
 #Return: The transcribed RNA string of t.
 
 t = open(".\\8_original.txt","r").read().replace("T", "U")
-#t = t.replace("T", "U")
 result = open("8_results","w").write(t).close()
-#result.close()
