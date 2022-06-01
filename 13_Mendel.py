3a514a0dfa74e853d5a9b8d31472f806e899c6ec
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 23:05:48 +0900

    Standardize

--- 13_Mendel.py
+++ 13_Mendel.py
@@ -1,5 +1,4 @@
 #Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
-
 #Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
 
 #k as first individual OR second individual results in 100% dominant phenotype
