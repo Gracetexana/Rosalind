a700dabc34b907a844320787b2a8274beb539581
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 22:54:04 +0900

    #Description

--- 10_rabbits.py
+++ 10_rabbits.py
@@ -1,5 +1,5 @@
-from re import M
-
+#Given: Positive integers n≤40 and k≤5.
+#Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
 
 n = 36
 k = 3
