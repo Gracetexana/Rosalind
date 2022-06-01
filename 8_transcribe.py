c52be06c358b0772661b9eb601b2f08e472747d9
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 22:49:53 +0900

    #Description

--- 8_transcribe.py
+++ 8_transcribe.py
@@ -1,3 +1,6 @@
+#Given: A DNA string t having length at most 1000 nt.
+#Return: The transcribed RNA string of t.
+
 t = open(".\\8_original.txt","r").read().replace("T", "U")
 #t = t.replace("T", "U")
 result = open("8_results","w").write(t).close()
