cc869d3e8b4c9b09a1e584a1cc4a1d711ce47a71
Gracetexana <65673263+Gracetexana@users.noreply.github.com>
2022-06-01 23:04:55 +0900

    #Description

--- 12_point.py
+++ 12_point.py
@@ -1,4 +1,5 @@
-#Given two strings s and t of equal length, the Hamming distance between s and t, denoted dH(s,t), is the number of corresponding symbols that differ in s and t
+#Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+#Return: The Hamming distance dH(s,t).
 
 f = open(".\\12_original.txt")
 c = 0
